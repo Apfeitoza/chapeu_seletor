dc39f11c04bae3f5413488e9d7838d6b0f70b46e
Apfeitoza <annapaulaasf@gmail.com>
2026-06-02 18:30:46 -0300

    feat(router): adiciona react-router-dom e a estrutura base de navegacao

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,18 @@
 import React from 'react';
-import NameInput from './components/NameInput';
-import useLocalStorage from './components/useLocalStorage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Quiz from './components/Quiz';
+import Resultado from './components/Resultado';
 
 const App = () => {
-  const [inputNome, setInputNome] = React.useState('');
-  const [nomeSalvo, setNomeSalvo] = useLocalStorage('name', '');
-  
-  function handleChange({ target }) {
-    setInputNome(target.value);
-  }
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    setNomeSalvo(inputNome);
-    setInputNome('');
-  }
-
-  if (nomeSalvo) {
-    return (
-      <div>
-        <h2>
-          Bem vindo a Hogwarts{' '}
-          <span style={{ color: 'purple' }}>{nomeSalvo}!</span>
-        </h2>
-        <button onClick={() => setNomeSalvo('')}>Limpar</button>
-      </div>
-    );
-  }
-
   return (
-    <NameInput
-      id={'nome'}
-      type={'text'}
-      input={inputNome}
-      onSubmit={handleSubmit}
-      onChange={handleChange}      
-    />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/resultado" element={<Resultado />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
